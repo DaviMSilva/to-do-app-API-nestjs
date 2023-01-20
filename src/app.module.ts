a5b43f2a2f17e97a7f5c163f4d853c9f66da58d2
Davi Silva <davisilvameneses@gmail.com>
2023-01-19 23:32:16 -0300

    nestjs api to do app

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
+
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://db_user:rB9DAMTPzvPuUn2l@cluster0.0ywlppf.mongodb.net/?retryWrites=true&w=majority',
+    ),
+    TasksModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
